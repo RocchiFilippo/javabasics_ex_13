97fce12ca4e0b1bc67284bf162b9c6dc3684f20f
FilippoRocchi <filipporocchi00@gmail.com>
2023-03-13 15:10:39 +0100

    ex 13

---
+++ _13/Main.java
@@ -0,0 +1,97 @@
+package _13;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: Add 8 random integers to the array list
+     * <p>
+     * Print out the first item
+     * <p>
+     * Remove the first 3 items
+     * <p>
+     * The print out the first remaining item
+     */
+    public static void exercise1() {
+        System.out.println("Exercise 1:");
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        //Write your code here
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(10);
+        numbers.add(12);
+        numbers.add(14);
+        numbers.add(16);
+
+        System.out.println("number in 0 position = " + numbers.get(0));
+
+        numbers.remove(0);
+        numbers.remove(0);
+        numbers.remove(0);
+
+        System.out.println("now, the number in 0 position is = " + numbers.get(0));
+
+    }
+
+    /**
+     * 2: Create 2 Array Lists,
+     * <p>
+     * A list of strings called 'menuItems'
+     * <p>
+     * And a list of Doubles called 'menuPrices'
+     * <p>
+     * The indexes of these two lists correspond to eachother
+     * <p>
+     * Create a print statement to output the name and price of each
+     * menu item correspond
+     */
+    public static void exercise2() {
+        System.out.println("\nExercise 1:");
+        //Write your code here
+
+        ArrayList<String> menuItems = new ArrayList<>();
+        ArrayList<Double> menuPrices = new ArrayList<>();
+
+        menuItems.add("Fries");
+        menuItems.add("hamburger");
+        menuItems.add("chickenburger");
+
+        menuPrices.add(3.99);
+        menuPrices.add(8.99);
+        menuPrices.add(7.99);
+
+        int myOrder = 1;
+
+        System.out.println("I've order " + menuItems.get(myOrder) + ", it's cost = " + menuPrices.get(myOrder));
+    }
+
+    /**
+     * 3: I changed this to an int array after recording!
+     * <p>
+     * You'll still need to find the total but using [index] instead of .get(index)
+     * <p>
+     * Find the total sum of all items in the ArrayList below.
+     * <p>
+     * Use the + operator
+     * <p>
+     * print the total
+     */
+    public static void exercise3() {
+        System.out.println("\nExercise 3:");
+        int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
+
+        //Write your code here
+        int total = items[0] + items[1] + items[2] + items[3] + items[4] + items[5] + items[6] + items[7] + items[8] + items[9];
+
+        System.out.println("the total sum of the values in the array is " + total);
+    }
+}
